82837ab33d12cf00e1bb0f5cb2334844dbce81fb
KimiMasa1021 <k.massan46@gmail.com>
2023-06-13 12:27:06 +0900

    iosでビルド通らない問題解消

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,6 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyDzB3j0TAQolKL9K-C_jqFQD6i3I_CHs9M")
-    GMSServices.setMetalRendererEnabled(true)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
